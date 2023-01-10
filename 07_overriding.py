1e354b82b719505f38bd0dc28a0326bd6a6cb1bd
shiva <116255391+shivagoud76@users.noreply.github.com>
2023-01-10 20:53:21 +0530

    Add files via upload

---
+++ 07_overriding.py
@@ -0,0 +1,10 @@
+class Parent:     # parent class or main class
+    def name(self):
+        print("It is a parent class")
+        
+class Child(Parent):    # child class or sub class
+    def name(self):
+        print('It is a child class')
+
+c=Child()
+c.name() 
